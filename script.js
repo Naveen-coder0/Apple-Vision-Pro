6b56b7b6bdf7a4d7eea02f0e7a3703773a51c682
Naveen Maan <naveen.maan2006@gmail.com>
2025-12-09 08:56:16 +0530

    Refactor Locomotive Scroll integration and clean up

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
 function loco() {
   gsap.registerPlugin(ScrollTrigger);
 
-  // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
 
   const locoScroll = new LocomotiveScroll({
     el: document.querySelector("#main"),
     smooth: true,
   });
-  // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+
   locoScroll.on("scroll", ScrollTrigger.update);
 
-  // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+
   ScrollTrigger.scrollerProxy("#main", {
     scrollTop(value) {
       return arguments.length
         ? locoScroll.scrollTo(value, 0, 0)
         : locoScroll.scroll.instance.scroll.y;
-    }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+    }, 
     getBoundingClientRect() {
       return {
         top: 0,
@@ -25,16 +24,16 @@ function loco() {
         height: window.innerHeight,
       };
     },
-    // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+
     pinType: document.querySelector("#main").style.transform
       ? "transform"
       : "fixed",
   });
 
-  // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll.
+ 
   ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
 
-  // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+  
   ScrollTrigger.refresh();
 }
 loco();
@@ -104,19 +103,7 @@ tl1.to("#page2>h1", {
   duration: 2,
 });
 
-// var tl2 = gsap.timeline({
-//   scrollTrigger: {
-//     trigger: `#page4`,
-//     start: `top top`,
-//     scrub: 1,
-//     scroller: `#main`,
-//     pin: true,
-//   },
-// });
 
-// tl2.to("#Page4>#center-page4", {
-//   top: `-50%`,
-// });
 
 var tl2 = gsap.timeline({
   scrollTrigger: {
@@ -519,7 +506,7 @@ function canvas1() {
     pin: true,
     // markers:true,
     scroller: `#main`,
-    //   set start end according to preference
+
     start: `top top`,
     end: `80% top`,
   });
